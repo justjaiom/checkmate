2d06c1e06f21c52d9f38ec5fbea233fb00d75979
Jaiom Rola <jaiomrola@gmail.com>
2024-10-12 21:36:55 -0500

    nav bar

--- website/src/components/navbar.jsx
+++ website/src/components/navbar.jsx
@@ -1,29 +1,50 @@
+import React, { useState } from 'react'
 
 function Navbar() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  console.log(isMenuOpen)
+
   return (
-    <nav className='bg-white px-8'> {/* Navbar background is white */}
+    <nav className='bg-white p-8'> 
       <div className="flex items-center justify-between">
-        {/* Logo */}
-        <div className="text-black text-2xl font-bold">
-        <img src={`${process.env.PUBLIC_URL}/Checkmate_logo.png`} alt="My PNG"  className="max-w-48"/>
 
-          </div> {/* Logo text is black */}
+        <div className="text-black text-2xl font-bold">Checkmate</div> 
 
-        {/* Navigation Links */}
+        <div className="md:hidden">
+          <button className='text-black' onClick={toggleMenu}>
+            <svg
+                fill='none'
+                stroke='currentColor'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                stroke-width='2'
+                viewBox='0 0 24 24'
+                className='w-6 h-6'
+            >
+                <path d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+        </div>
         <ul className='hidden md:flex space-x-4'>
           <li>
             <a 
-              href="#" 
-              className='bg-blue-600 text-white px-4 py-2 rounded' // Gradient background for Source Code button
+              href="https://github.com/justjaiom/checkmate" // Link to your GitHub repository
+              className='bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 text-white px-6 py-4 rounded' // Gradient background and 20px padding
               style={{ fontWeight: 'normal', fontSize: '0.875rem' }}
             >
-              Source Code
+Source Code
             </a>
           </li>
           <li>
             <a 
-              href="#" 
-              className='bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 text-white px-4 py-2 rounded' // Gradient background for Get Origin button
+              href="https://chrome.google.com/webstore" // Link to Google Chrome Extensions Store
+              className='bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 text-white px-6 py-4 rounded' // Gradient background and 20px padding
               style={{ fontWeight: 'normal', fontSize: '0.875rem' }}
             >
               Get Origin
@@ -31,6 +52,30 @@ function Navbar() {
           </li>
         </ul>
       </div>
+
+      {/* Mobile Menu */}
+      {isMenuOpen ? (
+        <ul className='flex-col md:hidden'>
+          <li className='py-2'>
+            <a 
+              href="https://github.com/justjaiom/checkmate" // Link to your GitHub repository
+              className='text-black px-6 py-4 rounded' // Gradient background and 20px padding
+              style={{ fontWeight: 'normal', fontSize: '0.875rem' }}
+            >
+              Source Code
+            </a>
+          </li>
+          <li className='py-2'>
+            <a 
+              href="https://chrome.google.com/webstore" // Link to Google Chrome Extensions Store
+              className='text-black px-6 py-4 rounded' // Gradient background and 20px padding
+              style={{ fontWeight: 'normal', fontSize: '0.875rem' }}
+            >
+              Get Origin
+            </a>
+          </li>
+        </ul>
+      ) : null}
     </nav>
   );
 }
